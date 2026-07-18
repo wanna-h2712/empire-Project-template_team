a2da31652cfeef89540c92d6117a08a4af05fdd8
Sudarat <sudaratimy@gmail.com>
2026-07-18 15:02:42 +0700

    finish weapon

--- weapon_shop/equip_item.py
+++ weapon_shop/equip_item.py
@@ -1,5 +1,5 @@
 # =====================================================
-#  weapon_shop/equip_item.py — คนรับผิดชอบ: ______________________
+#  weapon_shop/equip_item.py — คนรับผิดชอบ: อัยมี่
 #  หน้าที่: ซื้อและสวมใส่อาวุธให้ลูกน้อง (เช็คเงื่อนไข 2 อย่างก่อน)
 # =====================================================
 
@@ -9,14 +9,34 @@ def equip_item(person, weapon):
 #   - ผ่านทั้งคู่ -> หักเงิน, เปลี่ยน equipment เป็นชื่ออาวุธ, บวก bonus เข้า power
 #   - return {"status": True/False, "message": ข้อความบอกผล}
     # TODO: เขียนโค้ดตรงนี้
-    pass
+    dict_weapon = {"status": True, "message": "ระบบทำงาน"}
 
-
+    if person["money"] >= weapon["price"] :
+        if person["equipment"] == "ไม่มี" :
+            person["money"] = (int(person["money"] - weapon["price"]))
+            print(person["money"]) 
+            person["equipment"] = weapon["name"]
+            person["power"] = (int(person["power"] + weapon["bonus"]))
+            return dict_weapon
+        else :
+            return"ไม่สามารถสวมใส่ได้"
+    else :
+       return "เงินไม่พอ"
+# family_members = [
+#     {"name": "Tony", "age": 35, "role": "Hitman", "power": 7, "money": 5000000, "equipment": "ไม่มี"},
+#     {"name": "Luigi", "age": 28, "role": "Hitman", "power": 4, "money": 150000, "equipment": "ไม่มี"}
+# ]
+# weapons_catalog = {
+#     "1": {"name": "สนับมือ", "price": 10000, "bonus": 2},
+#     "2": {"name": "ปืนพก 9mm", "price": 50000, "bonus": 5},
+#     "3": {"name": "ปืนกล Thompson", "price": 150000, "bonus": 10}
+# }
 # ทดสอบเฉพาะไฟล์ตัวเอง: พิมพ์  python -m weapon_shop.equip_item
 if __name__ == "__main__":
     vito = {"name": "Vito", "money": 60000, "power": 5, "equipment": "ไม่มี"}
     gun = {"name": "ปืนพก 9mm", "price": 50000, "bonus": 5}
 
     print(equip_item(vito, gun))   # ต้องได้ status True
-    print(vito)                    # เงินเหลือ 10000, power เป็น 10, equipment เป็นปืน
-    print(equip_item(vito, gun))   # ครั้งที่สองต้องได้ "มีอาวุธอยู่แล้ว"
+    # print(vito)                    # เงินเหลือ 10000, power เป็น 10, equipment เป็นปืน
+    # print(equip_item(vito, gun))
+    # print(vito)   # ครั้งที่สองต้องได้ "มีอาวุธอยู่แล้ว"
--- weapon_shop/show_catalog.py
+++ weapon_shop/show_catalog.py
@@ -1,5 +1,5 @@
 # =====================================================
-#  weapon_shop/show_catalog.py — คนรับผิดชอบ: ______________________
+#  weapon_shop/show_catalog.py — คนรับผิดชอบ: ฟลุ๊ค______________________
 #  หน้าที่: แสดงรายการอาวุธทั้งหมดที่มีขาย
 # =====================================================
 from data import weapons_catalog
@@ -7,9 +7,28 @@ from data import weapons_catalog
 def show_catalog():
 #   - print อาวุธทุกชิ้นใน weapons_catalog บรรทัดละชิ้น (รหัส, ชื่อ, ราคา, พลังโบนัส)
     # TODO: เขียนโค้ดตรงนี้
-    pass
+    # print("name : สนับมือ - price : 10000 - bonus: 2")
+    # print("name : ปืนพก 9mm - price : 50000 - bonus : 5")
+    # print("name : ปืนกล Thompson - price : 150000 - bonus : 10")
+
+    for i in weapons_catalog :
+        print(f"{'1'} : {weapons_catalog["1"]}")
+        print(f"{'2'} : {weapons_catalog["2"]}")
+        print(f"{'3'} : {weapons_catalog["3"]}")
+print(show_catalog())
+
+
+# weapons_catalog = {
+#     "1": {"name": "สนับมือ", "price": 10000, "bonus": 2},
+#     "2": {"name": "ปืนพก 9mm", "price": 50000, "bonus": 5},
+#     "3": {"name": "ปืนกล Thompson", "price": 150000, "bonus": 10}
+# }
+
 
 
 # ทดสอบเฉพาะไฟล์ตัวเอง: พิมพ์  python -m weapon_shop.show_catalog
-if __name__ == "__main__":
-    show_catalog()   # ต้องเห็นอาวุธครบ 3 ชิ้น
+# if __name__ == "__main__":
+#     show_catalog()   # ต้องเห็นอาวุธครบ 3 ชิ้น
+
+
+# print (show_catalog())
